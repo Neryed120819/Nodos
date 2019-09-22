9ef7135ab57f71e8b1c0741542408ae41ad32419
Neryed120819 <eduardo11.agr@gmail.com>
2019-09-22 17:07:51 -0500

    Version 3.0

--- nodos.cpp
+++ nodos.cpp
@@ -1,91 +1,95 @@
 #include <iostream>
 #include <conio.h>
+#include <stdlib.h>
 using namespace std;
-
 
 struct nodo {
 	int valor;
-	nodo *puntero;
-	
+	struct nodo *puntero;
 };
 
-int main (int argc, char** argv){
-	
-	
-	struct nodo *inicio;
-	struct nodo *aux;
-	inicio=NULL;
-	//Mostrar el valor de un nodo
-	//aux=new nodo ();
-	//aux->valor=25;
-	//aux->puntero=NULL;
-	//cout<<aux->valor<<endl;
-	
-	//Mostrar los nodos con datos
-	struct nodo dato1, dato2, dato3, dato4, dato5;
-	dato1.valor=3;
-	dato1.puntero=NULL;
-	dato2.valor=7;
-	dato2.puntero=NULL;
-	dato3.valor=9;
-	dato3.puntero=NULL;
-	dato4.valor=10;
-	dato4.puntero=NULL;
-	dato5.valor=11;
-	dato5.puntero=NULL;
-	
-	
-	dato1.puntero=&dato2;
-	dato2.puntero=&dato3;
-	dato3.puntero=&dato4;
-	dato4.puntero=&dato5;
-	dato5.puntero=NULL;
-	
-	cout<<"\t"<<"Valor de nodos"<<"\t"<<endl;
-	cout<<dato1.valor<<endl;
-	cout<<dato1.puntero->valor<<endl;
-	cout<<dato1.puntero->puntero->valor<<endl;
-	cout<<dato1.puntero->puntero->puntero->valor<<endl;
-	cout<<dato1.puntero->puntero->puntero->puntero->valor<<endl;
-	
-	
-	//Mostrar nodos con el auxiliar
-	aux=new nodo();
-	aux->valor=12;
-	aux->puntero=NULL;
-	inicio=aux;
-	
-	aux=new nodo();
-	aux->valor=13;
-	aux->puntero=NULL;
-	inicio->puntero=aux;
-	
-	aux=new nodo();
-	aux->valor=14;
-	aux->puntero=NULL;
-	inicio->puntero->puntero=aux;
-	
-	aux=new nodo();
-	aux->valor=15;
-	aux->puntero=NULL;
-	inicio->puntero->puntero->puntero=aux;
-	
-	aux=new nodo();
-	aux->valor=16;
-	aux->puntero=NULL;
-	inicio->puntero->puntero->puntero->puntero=aux;
-	
-	cout<<"\t"<<"Valor de nodos"<<"\t"<<endl;
-	cout<<inicio->valor<<endl;
-	cout<<inicio->puntero->valor<<endl;
-	cout<<inicio->puntero->puntero->valor<<endl;
-	cout<<inicio->puntero->puntero->puntero->valor<<endl;
-	cout<<inicio->puntero->puntero->puntero->puntero->valor<<endl;
 
+//Se definen las funciones
+void menu();
+void agregar(nodo*&, int);
+void imprimir(nodo*);
 
-	
-	getch();
+int num, valor;
+nodo*lista=NULL;
+
+//INICIO 
+int main ()
+{
+	menu();
 	return 0;
+}
 	
+//Funcion del menu
+void menu(){
+	
+	do
+	{
+		cout<<"\t_|Menu|_"<<endl;
+		cout<<"1. Agregar nodo"<<endl;
+		cout<<"2. Imprimir nodo"<<endl;
+		cout<<"3. Salir"<<endl;
+		cout<<"Selecciona una opcion: ";
+		cin>>num;
+		
+		switch(num)
+		{
+			case 1:
+				cout<<"Ingresa un valor: ";
+				cin>>valor;
+				agregar(lista, valor);
+				cout<<"Valor guardado correctamente"<<endl;
+				system("pause");
+			break;
+			case 2:
+				cout<<"El valor guarado es: "<<endl;
+				imprimir(lista);
+				cout<<endl;
+				system("pause");
+			break;
+		}
+			
+		//system("cls");
+	}while (num!=3);
 }
 
+
+//Funcion agregar nodos
+void agregar(nodo*&lista, int dat)
+{
+	nodo*nuevoNod=new nodo();
+	nuevoNod->valor=dat;
+	nodo*aux1=lista;
+	nodo*aux2;
+	while ( (aux1!= NULL) && (aux1->valor<dat) )
+	{
+		aux2=aux1;
+		aux1=aux1->puntero;
+	}
+		if (lista==aux1)
+		{
+			lista=nuevoNod;
+		}
+		else{
+			aux2->puntero=nuevoNod;
+		}
+	nuevoNod->puntero=aux1;
+}
+
+
+
+//Funcion imprimir nodos
+void imprimir(nodo*lista)
+{
+	nodo*ac=new nodo();
+	ac=lista;
+	while (ac!= NULL)
+	{
+		cout<<"->"<<ac->valor;
+		ac=ac->puntero;
+	}
+}
